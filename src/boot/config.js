8f1fb7e430a089e9c5f05d4cd096a927740a9561
Miquel van Smoorenburg <mike@langeraar.net>
2022-01-14 01:18:14 +0100

    need boot/config.js

---
+++ src/boot/config.js
@@ -0,0 +1,22 @@
+//
+// boot file to read /config.json dynamically.
+// 404 not found is OK (for now).
+//
+export default async ({ store }) => {
+  const response = await fetch('/config.json', {
+    method: 'GET',
+    mode: 'same-origin',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    redirect: 'manual',
+    referrerPolicy: 'no-referrer',
+  });
+  if (!response.ok) {
+    if (response.status === 404) {
+      return;
+    }
+    throw new Error(`boot/config;js: GETting /config.json: error ${response.status}`);
+  }
+  const config = await response.json();
+  store.commit('remoteConfig', config);
+};
