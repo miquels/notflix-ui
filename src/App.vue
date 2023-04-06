4865c2ea765c53175a995b8a3401b28623a337f2
Miquel van Smoorenburg <mike@langeraar.net>
2023-04-07 00:40:23 +0200

    no disk cache for video files on mobile/tv

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,26 @@ import { defineComponent } from 'vue';
 import { useQuasar } from 'quasar';
 import { fixQuasarPlatform, fixQuasarCss } from 'src/lib/android-tv.js';
 
+var noVideoCacheOnDevice = false;
+
+const fetch = window.fetch;
+window.fetch = function(url, options) {
+  if (noVideoCacheOnDevice && (url.endsWith('.mp4') || url.endsWith('.m4a'))) {
+    options ||= {};
+    options.cache = 'no-store';
+  }
+  return fetch(url, options);
+};
+
 export default defineComponent({
   name: 'App',
   setup() {
     const $q = useQuasar();
     $q.dark.set(true);
     fixQuasarPlatform();
+    if ($q.platform.is.tv || $q.platform.is.mobile) {
+      noVideoCacheOnDevice = true;
+    }
   },
   mounted() {
     fixQuasarCss();
